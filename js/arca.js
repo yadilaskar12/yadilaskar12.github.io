279d45205b05f2b04d40d20168bf228006524276
rahmadriyadi <riyadirahmad164@gmail.com>
2019-07-19 00:17:14 +0700

    18072019

--- js/arca.js
+++ js/arca.js
@@ -6,21 +6,41 @@ $(document).ready(function(){
 			$(".navbar").removeClass('new');
 		}
 	});
-	$('#works').on('click',function(e){
+	$(".sc").on('click',function(e){
+		var href=$(this).attr('href');
+		var element=$(href);
+
+		if (href =="#work") {
+			$("html,body").animate({
+				scrollTop:Math.floor(element.offset().top -180)
+			},1000);
+		}else if (href =="#baru") {
+			$("html,body").animate({
+				scrollTop:Math.floor(element.offset().top +60)
+			},1000);
+		}
+		else{
+			$("html,body").animate({
+				scrollTop:Math.floor(element.offset().top -80)
+			},1000);
+		}
 		e.preventDefault();
-		$("html,body").animate({scrollTop:1800},1000);
 	});
-		$('#kontaks').on('click',function(e){
-		e.preventDefault();
-		$("html,body").animate({scrollTop:900},1000);
-	});
-		$('#home').on('click',function(e){
-		e.preventDefault();
-		$("html,body").animate({scrollTop:0},1000);
-	});
-			$('#baru').on('click',function(e){
-		e.preventDefault();
-		$("html,body").animate({scrollTop:1100},1000);
-	});
+	// $('#works').on('click',function(e){
+	// 	e.preventDefault();
+	// 	$("html,body").animate({scrollTop:1800},1000);
+	// });
+	// 	$('#kontaks').on('click',function(e){
+	// 	e.preventDefault();
+	// 	$("html,body").animate({scrollTop:900},1000);
+	// });
+	// 	$('#home').on('click',function(e){
+	// 	e.preventDefault();
+	// 	$("html,body").animate({scrollTop:0},1000);
+	// });
+	// 		$('#baru').on('click',function(e){
+	// 	e.preventDefault();
+	// 	$("html,body").animate({scrollTop:1100},1000);
+	// });
 
 });
